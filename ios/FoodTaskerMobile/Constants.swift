0479edfc43f6dc9d99808151f97a16ae253f6d15
scaaales <sergey.kletsov@outlook.com>
2017-12-22 07:28:56 +0200

    iOS Client

---
+++ ios/FoodTaskerMobile/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  FoodTaskerMobile
+//
+//  Created by Leo Trieu on 9/23/16.
+//  Copyright © 2016 Leo Trieu. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL: String = "http://localhost:8000"
+let CLIENT_ID: String = "tVs0Jfp851wTS9YeZKSi40U78jmzgpw2Hbh121iJ"
+let CLIENT_SECRET: String = "ZqAjODgInznH8nUymREcW8JlQgJeaVo84k2ct7GpmBTahUK6ljrOCMf34mARt3gBaw5Fd07z5Iw0y4yvS8N2Nvwc7pbr1g2Zd9bhEDgQEzXUJr9ZDwC4MtZKJSiTZSZ2"
+
+let USERTYPE_CUSTOMER = "customer"
+let USERTYPE_DRIVER = "driver"
+
+let STRIPE_PUBLIC_KEY = "pk_test_zqzqIUS8kzLRgZq21p9OjEhW"
